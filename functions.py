55a7581ee76fa8af7fba314994ed10b7a8b2423f
Viktor Kertanov <thelonius19@gmail.com>
2022-06-04 11:42:48 +0400

    Functions and main Python files, the initial commit

---
+++ functions.py
@@ -0,0 +1,5 @@
+def multiply(a,b):
+    return a*b
+
+def power(a,power):
+    return a**power
---
+++ main.py
@@ -0,0 +1,12 @@
+from functions import multiply, power
+
+
+a = 10
+b = 3
+my_power = 2
+
+c = multiply(a,b)
+
+final_result = power(c, my_power)
+
+print(f'Our final result is: {final_result}.')
