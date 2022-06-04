4aa38d295c901e82b53a6ab6122a66eeb47ba647
Viktor Kertanov <thelonius19@gmail.com>
2022-06-04 21:34:34 +0400

    Reverse string function added

--- functions.py
+++ functions.py
@@ -3,3 +3,12 @@ def multiply(a,b):
 
 def power(a,power):
     return a**power
+
+def reverse_eng(string):
+    return string[::-1]
+
+if __name__ == '__main__':
+    my_str = 'Витя, привет дорогой!'
+    rev_str = reverse_eng(my_str)
+    
+    print(rev_str)
