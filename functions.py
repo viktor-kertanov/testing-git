de61420e4b824f46915a70bf65fee1344f778333
viktor-kertanov <64532744+viktor-kertanov@users.noreply.github.com>
2022-06-04 22:09:39 +0400

    Feature/unique elements in list (#1)
    * Function for deduping lists implemented
    
    * Optimisation of the dedupe list function - made it shorter
    
    * Changed a string as requested in a review comment

--- functions.py
+++ functions.py
@@ -7,8 +7,16 @@ def power(a,power):
 def reverse_eng(string):
     return string[::-1]
 
+def unique_elements_of_list(my_list):
+    my_set = set(my_list)
+    return list(my_set)
+
 if __name__ == '__main__':
-    my_str = 'Витя, привет дорогой!'
+    my_str = 'Hello, World!'
     rev_str = reverse_eng(my_str)
-    
+
+    a = ['cat', 'dog', 'bog', 'sog', 'sog', 'rog', 'clog', 'dog', 'dog', 'cat']
+    print(f'Our list with no dupe check: {a}.')
+    print(f'No dupes: {unique_elements_of_list(a)}.')
+    print('-'*100)
     print(rev_str)
